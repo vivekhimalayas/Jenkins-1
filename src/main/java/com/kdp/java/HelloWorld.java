3e1598fc49f311e22ae76d2d7c11a1d4f33506a3
kdpthecoder <kpawar90@gmail.com>
2016-05-22 06:34:32 -0700

    added greet method()

--- src/main/java/com/kdp/java/HelloWorld.java
+++ src/main/java/com/kdp/java/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Jenkins!!!");
-	}
-
+		Greeting obj = new Greeting();
+		System.out.println(obj.greet("Jenkins"));
+	}	
 }
